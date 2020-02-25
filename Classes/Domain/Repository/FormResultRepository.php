98af45292aa0b245b81b8d82484bf1090cbe9873
Stig Nørgaard Færch <stigfaerch@gmail.com>
2020-02-25 21:10:04 +0100

    [BUGFIX] Reimplement fix - "Can't get site-identifiers as admin or if the identifier is on page root (ID:0)" - fixing: access to a subpage givse access to all results from that site.

--- Classes/Domain/Repository/FormResultRepository.php
+++ Classes/Domain/Repository/FormResultRepository.php
@@ -12,11 +12,11 @@ use DateInterval;
 use DateTime;
 use Exception;
 use Lavitto\FormToDatabase\Utility\FormValueUtility;
+use PDO;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Database\QueryGenerator;
 use TYPO3\CMS\Core\Exception\SiteNotFoundException;
-use TYPO3\CMS\Core\Routing\SiteMatcher;
 use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException;
@@ -58,6 +58,7 @@ class FormResultRepository extends Repository
      *
      * @param string $formPersistenceIdentifier
      * @return QueryResultInterface
+     * @throws InvalidQueryException
      */
     public function findByFormPersistenceIdentifier(string $formPersistenceIdentifier): QueryResultInterface
     {
@@ -69,6 +70,7 @@ class FormResultRepository extends Repository
      *
      * @param string $formPersistenceIdentifier
      * @return int
+     * @throws InvalidQueryException
      */
     public function countByFormPersistenceIdentifier(string $formPersistenceIdentifier): int
     {
@@ -86,44 +88,50 @@ class FormResultRepository extends Repository
     {
         $query = $this->createQuery();
         $webMounts = $this->getWebMounts();
-        $siteIdentifiers = $this->getSiteIdentifiersFromRootPids($webMounts);
-        $pluginUids = $this->getPluginUids($webMounts);
+        if (empty($webMounts) === false) {
+            $siteIdentifiers = $this->getSiteIdentifiersFromRootPids($webMounts);
+            $pluginUids = $this->getPluginUids($webMounts);
+            $orConditions = [];
+            // Include result if user has access to the plugin which the result originates
+            if($siteIdentifiers) $orConditions[] = $query->in('formPluginUid', $pluginUids);
+            // Include result if user has root access to site
+            if($pluginUids) $orConditions[] = $query->in('siteIdentifier', $siteIdentifiers);
+            // Includes result if result is old (those created before new identifying fields)
+            $orConditions[] = $query->logicalAnd([
+                $query->equals('siteIdentifier', ''),
+                $query->equals('pid', 0)
+            ]);
+            // Include result always if user is admin
+            if($GLOBALS['BE_USER']->isAdmin()) $orConditions[] = $query->greaterThan('uid', 0);
 
-        $query->matching(
-            $query->logicalAnd([
-                $query->equals('formPersistenceIdentifier', $formPersistenceIdentifier),
-                $query->logicalOr([
-                    $query->in('formPluginUid', $pluginUids),
-                    $query->in('siteIdentifier', $siteIdentifiers),
-                    //To include all records created before pid and siteIdentifier was taken into account
-                    $query->logicalAnd([
-                        $query->equals('siteIdentifier', ''),
-                        $query->equals('pid', 0)
-                    ])
+            $query->matching(
+                $query->logicalAnd([
+                    $query->equals('formPersistenceIdentifier', $formPersistenceIdentifier),
+                    $query->logicalOr($orConditions)
                 ])
-            ])
-        );
+            );
+        }
         return $query;
     }
 
     /**
-     * Get webmounts of BE User
+     * Get webMounts of BE User
      *
      * @return array
      */
-    protected function getWebMounts () {
-        $webMounts = [];
-        if ($GLOBALS['BE_USER']->groupData['webmounts']) {
-            $webMounts = GeneralUtility::trimExplode(',', $GLOBALS['BE_USER']->groupData['webmounts'], 1);
-        }
-        return $webMounts;
+    protected function getWebMounts(): array
+    {
+        return $GLOBALS['BE_USER']->returnWebmounts();
     }
 
     /**
-     * @param $webMounts
+     * Gets the plugin uids
+     *
+     * @param array $webMounts
      * @return array
      */
-    protected function getPluginUids($webMounts) {
+    protected function getPluginUids(array $webMounts): array
+    {
         $pids = $this->getTreePids($webMounts);
         /** @var QueryBuilder $queryBuilder */
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
@@ -133,7 +141,8 @@ class FormResultRepository extends Repository
             ->from('tt_content')
             ->where(
                 $queryBuilder->expr()->in('pid', $pids),
-                $queryBuilder->expr()->eq('CType', $queryBuilder->createNamedParameter('form_formframework', \PDO::PARAM_STR))
+                $queryBuilder->expr()->eq('CType',
+                    $queryBuilder->createNamedParameter('form_formframework', PDO::PARAM_STR))
             )
             ->execute()->fetchAll();
         return array_column($result, 'uid');
@@ -145,37 +154,40 @@ class FormResultRepository extends Repository
      * @param array $webMounts
      * @return array
      */
-    function getTreePids($webMounts = 0){
-        $childPidsArray = [];
-        if($webMounts) {
+    protected function getTreePids(array $webMounts): array
+    {
+        $pidsArray = [];
+        if($webMounts !== null) {
             $depth = 99;
-            $childPidsArray = [];
+            $pidsArray = [];
             /** @var QueryGenerator $queryGenerator */
             $queryGenerator = GeneralUtility::makeInstance( QueryGenerator::class );
             foreach ($webMounts as $webMount) {
                 $childPids = $queryGenerator->getTreeList($webMount, $depth, 0, 1); //Will be a string like 1,2,3
-                $childPidsArray = array_merge($childPidsArray, explode(',', $childPids ));
+                $pidsArray = array_merge($pidsArray, explode(',', $childPids ));
             }
         }
-        return array_unique($childPidsArray);
+        return array_unique($pidsArray);
     }
 
     /**
      * Get SiteIdentifiers from Root Pids
      *
+     * @param array $webMounts
      * @return array
      */
-    protected function getSiteIdentifiersFromRootPids($webMounts) {
+    protected function getSiteIdentifiersFromRootPids(array $webMounts): array
+    {
         $siteIdentifiers = [];
         if ($webMounts) {
-            //find site identifiers from mountpoints
+            // find site identifiers from mountpoints
             /** @var SiteFinder $siteMatcher */
             $siteMatcher = GeneralUtility::makeInstance(SiteFinder::class);
             foreach ($webMounts as $webMount) {
                 try {
                     $site = $siteMatcher->getSiteByRootPageId((int)$webMount);
                     $siteIdentifiers[] = $site->getIdentifier();
-                } catch (SiteNotFoundException $exception) {}
+                    } catch (SiteNotFoundException $exception) {}
             }
         }
         return $siteIdentifiers;
