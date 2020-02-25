94835e21e05c62881e2ba32f854c4aece6b39f92
Stig Nørgaard Færch <stigfaerch@gmail.com>
2020-02-25 16:26:50 +0100

    Revert "[BUGFIX] Can't get site-identifiers as admin or if the identifier is on page root (ID: 0)"
    This reverts commit fd5866769fa0718f897b10072dd45e1033d2e7f9.

--- Classes/Domain/Repository/FormResultRepository.php
+++ Classes/Domain/Repository/FormResultRepository.php
@@ -12,12 +12,11 @@ use DateInterval;
 use DateTime;
 use Exception;
 use Lavitto\FormToDatabase\Utility\FormValueUtility;
-use PDO;
-use TYPO3\CMS\Backend\Tree\Repository\PageTreeRepository;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Database\QueryGenerator;
 use TYPO3\CMS\Core\Exception\SiteNotFoundException;
-use TYPO3\CMS\Core\Site\Entity\Site;
+use TYPO3\CMS\Core\Routing\SiteMatcher;
 use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException;
@@ -44,13 +43,6 @@ class FormResultRepository extends Repository
     ];
 
     /**
-     * The PageTreeRepository
-     *
-     * @var PageTreeRepository
-     */
-    protected $pageTreeRepository;
-
-    /**
      * Ignore storage pid
      */
     public function initializeObject(): void
@@ -62,21 +54,10 @@ class FormResultRepository extends Repository
     }
 
     /**
-     * Injects the PageTreeRepository
-     *
-     * @param PageTreeRepository $pageTreeRepository
-     */
-    public function injectPageTreeRepository(PageTreeRepository $pageTreeRepository): void
-    {
-        $this->pageTreeRepository = $pageTreeRepository;
-    }
-
-    /**
      * Gets all results by form definition
      *
      * @param string $formPersistenceIdentifier
      * @return QueryResultInterface
-     * @throws InvalidQueryException
      */
     public function findByFormPersistenceIdentifier(string $formPersistenceIdentifier): QueryResultInterface
     {
@@ -88,7 +69,6 @@ class FormResultRepository extends Repository
      *
      * @param string $formPersistenceIdentifier
      * @return int
-     * @throws InvalidQueryException
      */
     public function countByFormPersistenceIdentifier(string $formPersistenceIdentifier): int
     {
@@ -106,45 +86,44 @@ class FormResultRepository extends Repository
     {
         $query = $this->createQuery();
         $webMounts = $this->getWebMounts();
-        if (empty($webMounts) === false) {
-            $siteIdentifiers = $this->getSiteIdentifiersFromRootPids($webMounts);
-            $pluginUids = $this->getPluginUids($webMounts);
-            $query->matching(
-                $query->logicalAnd([
-                    $query->equals('formPersistenceIdentifier', $formPersistenceIdentifier),
-                    $query->logicalOr([
-                        $query->in('formPluginUid', $pluginUids),
-                        $query->in('siteIdentifier', $siteIdentifiers),
-                        // To include all records created before pid and siteIdentifier was taken into account
-                        $query->logicalAnd([
-                            $query->equals('siteIdentifier', ''),
-                            $query->equals('pid', 0)
-                        ])
+        $siteIdentifiers = $this->getSiteIdentifiersFromRootPids($webMounts);
+        $pluginUids = $this->getPluginUids($webMounts);
+
+        $query->matching(
+            $query->logicalAnd([
+                $query->equals('formPersistenceIdentifier', $formPersistenceIdentifier),
+                $query->logicalOr([
+                    $query->in('formPluginUid', $pluginUids),
+                    $query->in('siteIdentifier', $siteIdentifiers),
+                    //To include all records created before pid and siteIdentifier was taken into account
+                    $query->logicalAnd([
+                        $query->equals('siteIdentifier', ''),
+                        $query->equals('pid', 0)
                     ])
                 ])
-            );
-        }
+            ])
+        );
         return $query;
     }
 
     /**
-     * Get webMounts of BE User
+     * Get webmounts of BE User
      *
      * @return array
      */
-    protected function getWebMounts(): array
-    {
-        return $GLOBALS['BE_USER']->returnWebmounts();
+    protected function getWebMounts () {
+        $webMounts = [];
+        if ($GLOBALS['BE_USER']->groupData['webmounts']) {
+            $webMounts = GeneralUtility::trimExplode(',', $GLOBALS['BE_USER']->groupData['webmounts'], 1);
+        }
+        return $webMounts;
     }
 
     /**
-     * Gets the plugin uids
-     *
-     * @param array $webMounts
+     * @param $webMounts
      * @return array
      */
-    protected function getPluginUids(array $webMounts): array
-    {
+    protected function getPluginUids($webMounts) {
         $pids = $this->getTreePids($webMounts);
         /** @var QueryBuilder $queryBuilder */
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
@@ -154,8 +133,7 @@ class FormResultRepository extends Repository
             ->from('tt_content')
             ->where(
                 $queryBuilder->expr()->in('pid', $pids),
-                $queryBuilder->expr()->eq('CType',
-                    $queryBuilder->createNamedParameter('form_formframework', PDO::PARAM_STR))
+                $queryBuilder->expr()->eq('CType', $queryBuilder->createNamedParameter('form_formframework', \PDO::PARAM_STR))
             )
             ->execute()->fetchAll();
         return array_column($result, 'uid');
@@ -167,60 +145,37 @@ class FormResultRepository extends Repository
      * @param array $webMounts
      * @return array
      */
-    protected function getTreePids(array $webMounts): array
-    {
-        $pidsArray = [];
-        if ($webMounts !== null) {
+    function getTreePids($webMounts = 0){
+        $childPidsArray = [];
+        if($webMounts) {
+            $depth = 99;
+            $childPidsArray = [];
+            /** @var QueryGenerator $queryGenerator */
+            $queryGenerator = GeneralUtility::makeInstance( QueryGenerator::class );
             foreach ($webMounts as $webMount) {
-                $pageTree = $this->pageTreeRepository->getTree((int)$webMount);
-                $this->addTreeToPidsArray($pidsArray, $pageTree);
+                $childPids = $queryGenerator->getTreeList($webMount, $depth, 0, 1); //Will be a string like 1,2,3
+                $childPidsArray = array_merge($childPidsArray, explode(',', $childPids ));
             }
         }
-        return array_unique($pidsArray);
-    }
-
-    /**
-     * Adds the uids of pages from pagetree to an array
-     *
-     * @param $pidsArray
-     * @param array $pageTree
-     */
-    protected function addTreeToPidsArray(&$pidsArray, array $pageTree): void
-    {
-        $pidsArray[] = $pageTree['uid'];
-        if (empty($pageTree['_children']) === false) {
-            foreach ($pageTree['_children'] as $children) {
-                $this->addTreeToPidsArray($pidsArray, $children);
-            }
-        }
+        return array_unique($childPidsArray);
     }
 
     /**
      * Get SiteIdentifiers from Root Pids
      *
-     * @param array $webMounts
      * @return array
      */
-    protected function getSiteIdentifiersFromRootPids(array $webMounts): array
-    {
+    protected function getSiteIdentifiersFromRootPids($webMounts) {
         $siteIdentifiers = [];
         if ($webMounts) {
-            // find site identifiers from mountpoints
+            //find site identifiers from mountpoints
             /** @var SiteFinder $siteMatcher */
             $siteMatcher = GeneralUtility::makeInstance(SiteFinder::class);
             foreach ($webMounts as $webMount) {
-                if ((int)$webMount === 0) {
-                    /** @var Site $site */
-                    foreach ($siteMatcher->getAllSites() as $site) {
-                        $siteIdentifiers[] = $site->getIdentifier();
-                    }
-                } else {
-                    try {
-                        $site = $siteMatcher->getSiteByPageId((int)$webMount);
-                        $siteIdentifiers[] = $site->getIdentifier();
-                    } catch (SiteNotFoundException $exception) {
-                    }
-                }
+                try {
+                    $site = $siteMatcher->getSiteByRootPageId((int)$webMount);
+                    $siteIdentifiers[] = $site->getIdentifier();
+                } catch (SiteNotFoundException $exception) {}
             }
         }
         return $siteIdentifiers;
