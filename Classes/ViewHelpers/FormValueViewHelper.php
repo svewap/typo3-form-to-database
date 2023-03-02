1bd6ebc130e583c987c32797a340716f916b5bd9
Stig Nørgaard Færch <stigfaerch@gmail.com>
2023-03-02 07:24:45 +0000

    [BUGFIX] Error in result list if record did not have field
    (cherry picked from commit 1dc035099c64a3c096977e76d136fad2528e3470)

--- Classes/ViewHelpers/FormValueViewHelper.php
+++ Classes/ViewHelpers/FormValueViewHelper.php
@@ -46,7 +46,7 @@ class FormValueViewHelper extends AbstractViewHelper
      */
     public function render(): string
     {
-        return FormValueUtility::convertFormValue($this->arguments['element'], $this->arguments['results'][$this->arguments['value']],
+        return FormValueUtility::convertFormValue($this->arguments['element'], $this->arguments['results'][$this->arguments['value']] ?? '',
             FormValueUtility::OUTPUT_TYPE_HTML, $this->arguments['crop'] === true);
     }
 }
