3cc9b9e6b6ee3eb9361f01e199a56c9dbea84b16
Stig Nørgaard Færch <stigfaerch@gmail.com>
2023-03-13 13:33:23 +0100

    [TASK] Revert dependency injection configuration to regain v9 compatibility

--- Classes/Hooks/FormHooks.php
+++ Classes/Hooks/FormHooks.php
@@ -15,10 +15,15 @@ use Lavitto\FormToDatabase\Domain\Repository\FormResultRepository;
 use Lavitto\FormToDatabase\Utility\UniqueFieldHandler;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Resource\StorageRepository;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException;
 use TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException;
 use TYPO3\CMS\Extbase\Persistence\Generic\QueryResult;
+use TYPO3\CMS\Form\Mvc\Configuration\YamlSource;
 use TYPO3\CMS\Form\Mvc\Persistence\FormPersistenceManager;
+use TYPO3\CMS\Form\Mvc\Persistence\FormPersistenceManagerInterface;
 
 /**
  * Class FormHooks
@@ -27,36 +32,49 @@ use TYPO3\CMS\Form\Mvc\Persistence\FormPersistenceManager;
  */
 class FormHooks
 {
-
     /**
-     * @var UniqueFieldHandler
+     * @var ObjectManager
      */
-    protected UniqueFieldHandler $uniqueFieldHandler;
+    protected  $objectManager;
 
     /**
      * @var FormPersistenceManager
      */
-    protected FormPersistenceManager $formPersistenceManager;
+    protected  $formPersistenceManager;
 
     /**
      * The FormResultRepository
      *
      * @var FormResultRepository
      */
-    public FormResultRepository $formResultRepository;
+    public  $formResultRepository;
 
     /**
-     * @param UniqueFieldHandler $uniqueFieldHandler
-     * @param FormPersistenceManager $formPersistenceManager
-     * @param FormResultRepository $formResultRepository
+     * Injects the FormResultRepository
      */
-    public function __construct(UniqueFieldHandler $uniqueFieldHandler, FormPersistenceManager $formPersistenceManager, FormResultRepository $formResultRepository)
+    public function initializeFormResultRepository(): void
     {
-        $this->uniqueFieldHandler = $uniqueFieldHandler;
-        $this->formPersistenceManager = $formPersistenceManager;
-        $this->formResultRepository = $formResultRepository;
+        $this->formResultRepository = $this->objectManager->get(FormResultRepository::class);
     }
 
+    /**
+     * Injects necessary objects into the formPersistenceManager
+     */
+    protected function initializeFormPersistenceManager(): void
+    {
+        /** @var FormPersistenceManagerInterface $formPersistenceManager */
+        $this->formPersistenceManager = GeneralUtility::makeInstance(FormPersistenceManager::class);
+        $this->formPersistenceManager->initializeObject();
+        $this->formPersistenceManager->injectResourceFactory(GeneralUtility::makeInstance(ResourceFactory::class));
+
+        /** @var StorageRepository $storageRepository */
+        $storageRepository = $this->objectManager->get(StorageRepository::class);
+        $this->formPersistenceManager->injectStorageRepository($storageRepository);
+
+        /** @var YamlSource $yamlSource */
+        $yamlSource = $this->objectManager->get(YamlSource::class);
+        $this->formPersistenceManager->injectYamlSource($yamlSource);
+    }
 
     /**
      * @param $formPersistenceIdentifier
@@ -70,6 +88,9 @@ class FormHooks
      */
     public function beforeFormDelete($formPersistenceIdentifier): void
     {
+        $this->objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+        $this->initializeFormResultRepository();
+        $this->initializeFormPersistenceManager();
         $yaml = $this->formPersistenceManager->load($formPersistenceIdentifier);
 
         /** @var File $file */
@@ -110,6 +131,8 @@ class FormHooks
      */
     public function beforeFormSave($formPersistenceIdentifier, $formDefinition)
     {
-        return $this->uniqueFieldHandler->updateNewFields($formPersistenceIdentifier, $formDefinition);
+        $this->objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+
+        return  $this->objectManager->get(UniqueFieldHandler::class)->updateNewFields($formPersistenceIdentifier, $formDefinition);
     }
 }
